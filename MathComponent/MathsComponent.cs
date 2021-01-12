6ad473e019d21ed2fa4e4290e30a30c9d5b3e6b2
alibouziane <alibouziane@DESK-ALIBOUZIAN>
2021-01-12 18:54:14 +0100

    Add project files.

---
+++ MathComponent/MathsComponent.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace MathComponent
+{
+    public class MathsComponent
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Substruct(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public string FizzBuz(int value)
+        {
+            if (value >= 1000)
+                throw new Exception("Not Allowed beacause it is Greather Than 100");
+            if (value % 5 == 0 && value % 3 == 0) return "FizBuzz";
+            if (value % 5 == 0) return "Buzz";
+            else if (value % 3 == 0) return "Fizz";
+            else return value.ToString();
+        }
+
+
+        public bool IsPalindrom(string input)
+        {
+            if (input == null) return false;
+            var min = 0;
+            var max = input.Length - 1;
+
+            while (min < max)
+            {
+                if (input[min] != input[max]) return false;
+                min++;
+                max--;
+            }
+            return true;
+        }
+
+        public bool IsPalindrom2(string input)
+        {
+            return input != null && input.SequenceEqual(input.Reverse());
+        }
+
+        public int StringCalculator(string input)
+        {
+
+            var inputs = input.Split('\n', ',');
+
+            return inputs.Sum(int.Parse);
+
+        }
+    }
+}
---
+++ MathsTestProject/UnitTest1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using MathComponent;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace MathsTestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestAdd()
+        {
+            var obj = new MathsComponent();
+            int result = obj.Add(10, 10);
+            Assert.AreEqual(20, result);
+        }
+
+        [TestMethod]
+        public void TestSubstruct()
+        {
+            var obj = new MathsComponent();
+            int result = obj.Substruct(10, 10);
+            Assert.AreEqual(0, result);
+        }
+
+        [TestMethod]
+        public void WHenMultipleFiveReturnBuzz()
+        {
+            var obj = new MathsComponent();
+            string result = obj.FizzBuz(5);
+            Assert.AreEqual(result, "Buzz");
+        }
+
+        [TestMethod]
+        public void WhenMultipleThreeReturnFizz()
+        {
+            var obj = new MathsComponent();
+            string result = obj.FizzBuz(3);
+            Assert.AreEqual(result, "Fizz");
+        }
+
+        [TestMethod]
+        public void WHenMultipleFiveAndMultipleThreeThanReturnFizzBuzz()
+        {
+            var obj = new MathsComponent();
+            string result = obj.FizzBuz(15);
+            Assert.AreEqual(result, "FizBuzz");
+        }
+
+        [TestMethod]
+        public void WhenGreatherThan100ReturnException()
+        {
+            var obj = new MathsComponent();
+            try
+            {
+                string result = obj.FizzBuz(1000);
+                Assert.Fail("no exception thrown");
+            }
+            catch (Exception ex)
+            {
+                Assert.IsTrue(ex.Message == "Not Allowed beacause it is Greather Than 100");
+            }
+        }
+
+        [TestMethod]
+        public void IspalindromTest()
+        {
+            var obj = new MathsComponent();
+            bool result = obj.IsPalindrom("civic");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void IspalindromTest2()
+        {
+            var obj = new MathsComponent();
+            bool result = obj.IsPalindrom2("civic");
+            Assert.IsTrue(result);
+        }
+        [TestMethod]
+        public void WhenNumbersSeparatedWithEitherNewLineANdCommaReturnSum()
+        {
+            string input = "3\n4,-2";
+            int expected = 5;
+
+            var obj = new MathsComponent();
+            int result = obj.StringCalculator(input);
+            Assert.AreEqual(result, expected);
+
+
+        }
+
+    }
+}
